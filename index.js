0d9fcc1d9f3b719eaba49c5dbf38e768e7cf1076
tinkitwong <tinkitwong@gmail.com>
2020-03-28 15:14:08 +0800

    Added Chat Functionality

--- index.js
+++ index.js
@@ -2,12 +2,27 @@ var app = require('express')();
 // express initializes the app to be a function handler that you can supply to a HTTP server
 var http = require('http').createServer(app);
 
+// initialize a new instance of socket.io by passing http server object
+var io = require('socket.io')(http);
+
 // define a route handler / that gets called when we hit a website home
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+        res.sendFile(__dirname + '/index.html');
 });
+
+// listen for connectione events for incoming sockets
+io.on('connection', function(socket){
+      //log to console
+      console.log('a user connected');
+//      socket.broadcast.emit('hi');
+      socket.on('chat message', function(msg){
+                //console.log('message :' + msg);
+                io.emit('chat message', msg);
+                });
+      
+      });
 
 // make the http server listen on port 3000
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+            console.log('listening on *:3000');
 });
